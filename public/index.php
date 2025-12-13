4417247fe9c25ba050e7c030641b9ad9dcd70d9c
SkyWors <skyworslegrand@gmail.com>
2025-12-13 17:06:27 +0100

    ✨ Add renderer modules, update Tempora's logo

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+use Tempora\Tempora;
+use Tempora\Utils\Render\Modules\RenderRemoveWhitespaceBetweenTagsModule;
+use Tempora\Utils\Render\Modules\RenderRemoveTrailingWhitespaceModule;
+use Tempora\Utils\Render\Modules\RenderRemoveEmptyLinesModule;
+use Tempora\Utils\Render\Modules\RenderCollapseSpacesModule;
+use Tempora\Utils\Render\Modules\RenderRemoveNewLinesModule;
+use Tempora\Utils\Render\Modules\RenderRemoveCommentsModule;
+
 // Path
 define(constant_name: "APP_DIR", value: $_SERVER["DOCUMENT_ROOT"] . "/..");
 
@@ -7,13 +15,13 @@ define(constant_name: "APP_DIR", value: $_SERVER["DOCUMENT_ROOT"] . "/..");
 require APP_DIR . "/vendor/autoload.php";
 
 // Tempora's kernel
-$options = [
-	"remove_whitespace_between_tags",
-	"remove_trailing_whitespace",
-	"remove_empty_lines",
-	"remove_new_lines",
-	"remove_comments",
-	"collapsed_spaces",
-];
-
-new Tempora\Tempora(options: $options);
+(new Tempora(
+	modules: [
+		new RenderRemoveWhitespaceBetweenTagsModule(),
+		new RenderRemoveTrailingWhitespaceModule(),
+		new RenderRemoveEmptyLinesModule(),
+		new RenderCollapseSpacesModule(),
+		new RenderRemoveNewLinesModule(),
+		new RenderRemoveCommentsModule()
+	]
+));
